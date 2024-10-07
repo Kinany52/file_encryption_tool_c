c9032ea5013d1f1876f131e4185addf2a1bc15a4
Sarmad Al Kinany <sarmad.alkinany@tivian.com>
2024-10-07 11:05:36 +0200

    var declarations + std input&output

--- src/main.c
+++ src/main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
 void main() {
+    int choice;
+    char inputFilePath[256], outputFilePath[256];
     //void for now
-    printf(" c implementation of a previously built app.\n");
+    printf("File Encryption/Decryption Tool\n");
+    printf("1. Encrypt a file\n");
+    printf("2. Decrypt a file\n");
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+
+    printf("Enter input file path: ");
+    scanf("%s", inputFilePath);
+
+    printf("Enter output file path: ");
+    scanf("%s", outputFilePath);
+    
 }
